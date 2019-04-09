69331a4a92379965621c2b423d8df9bcc27a8e90
Carlos Calderon <calderoncarloos@gmail.com>
2019-04-08 22:27:23 -0400

    HTTPS?

--- iFinance/App_Start/FilterConfig.cs
+++ iFinance/App_Start/FilterConfig.cs
@@ -8,6 +8,8 @@ namespace iFinance
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            //Force all request to use ssl
+            filters.Add(new RequireHttpsAttribute());
         }
     }
 }
